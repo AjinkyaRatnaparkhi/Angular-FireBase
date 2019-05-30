92ac078961d70ef4abc7a4060315f12478566cfb
Ajinkya Ratnaparkhi <ajinkya.s.ratnaparkhi@gmail.com>
2019-05-30 00:25:03 -0400

    updated configuration

--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCYaGbDltyDnXf1isKYWTSY5BgPePUQEF0",
-    authDomain: "ng-fitness-d7fc8.firebaseapp.com",
-    databaseURL: "https://ng-fitness-d7fc8.firebaseio.com",
-    projectId: "ng-fitness-d7fc8",
-    storageBucket: "ng-fitness-d7fc8.appspot.com",
-    messagingSenderId: "940919863095",
-    appId: "1:940919863095:web:085118a6d8c2f967"
-  }
+  production: true
 };
--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false ,
-  firebase: {
-    apiKey: "AIzaSyCYaGbDltyDnXf1isKYWTSY5BgPePUQEF0",
-    authDomain: "ng-fitness-d7fc8.firebaseapp.com",
-    databaseURL: "https://ng-fitness-d7fc8.firebaseio.com",
-    projectId: "ng-fitness-d7fc8",
-    storageBucket: "ng-fitness-d7fc8.appspot.com",
-    messagingSenderId: "940919863095",
-    appId: "1:940919863095:web:085118a6d8c2f967"
-  }
+  production: false
 };
 
 /*
